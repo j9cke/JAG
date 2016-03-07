9a86b301db638eee6ee797ab56011eb0bca938c3
Gustav Carlsson <gustavcarlsson9@gmail.com>
2016-03-07 16:10:51 +0100

    Validation för Author

--- JAGLib/Common/Models/Author.cs
+++ JAGLib/Common/Models/Author.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Common.Models
 {
     public class Author
     {
         public int _id { get; set; }
+
+        [Required(ErrorMessage = "Firstname is required.")]
         public string _firstname { get; set; }
+
+        [Required(ErrorMessage = "Lastname is required.")]
         public string _lastname { get; set; }
+
+        [Required(ErrorMessage = "Birthyear is required.")]
+        [MinLength(4), MaxLength(4)]
         public string _birthyear { get; set; }
     }
 }
