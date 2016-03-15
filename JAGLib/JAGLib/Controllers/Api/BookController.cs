b3207872c5069985ecfe644d6d88d72bf7907160
j9cke <joacimfloren@gmail.com>
2016-03-15 14:30:34 +0100

    början på API & en buggfix

---
+++ JAGLib/JAGLib/Controllers/Api/BookController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Service.Services;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace JAGLib.Controllers.Api
+{
+    public class BookController : Controller
+    {
+        //public HttpResponseMessage GetListOfBooks(string s)
+        //{
+        //    // Code handling GET requests for /api/Book
+        //    HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, BookServices.searchBook(s));
+        //    response.ReasonPhrase = "OK";
+        //    return response;
+        //}
+
+        public string Search(string id)
+        {
+            return JsonConvert.SerializeObject(BookServices.searchBook(id));
+        }
+    }
+}
